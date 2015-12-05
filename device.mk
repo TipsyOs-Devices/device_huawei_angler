ee2f6489dea7fbf472ee2b58338ccf60aba53c6b
DHOMD <DHO@dho.im>
2015-12-05 12:19:51 -0500

    angler: squash of telephony fixes [3]
    Author: DHOMD <DHO@dho.im>
    Date:   Sat Dec 5 12:19:51 2015 -0500
        Enable IMSI from Sim
        Fixes CDMA Roamig issues.
        Confirmed as fix on verizon
        Change-Id: If2c23527fca347326b4b52a5b1cee2166426115b
    
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Wed Nov 25 11:07:41 2015 -0500
        Enable change from CdmaLteServiceStateTracker: allow forcing reading eri from xml
        Fixes cibdition where mobile network tile shows 123456 for the operator on verizon 4g
        Change-Id: I22db74fe6bc9c363b8eac39fb30607f9a4707814
        Signed-off-by: DHOMD <DHO@dho.im>
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Jan 8 01:42:31 2016 -0600
        angler: Enable tethering
        Change-Id: Ic0869cbe0cc4a1d5e56e41953952e5eb2dc336f0
    
    Change-Id: I1ab36f0ad7e03bda0fea41b0ed39bca76be7a02f

--- device.mk
+++ device.mk
@@ -313,6 +313,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.force_eri_from_xml=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.force_eri_from_xml=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=72 \
     ro.hwui.layer_cache_size=48 \
     ro.hwui.r_buffer_cache_size=8 \
