740cf9c4d968b51bb20b900ddd07091e2645e9c2
Dustin Rinne <dustin.rinne@gmail.com>
2016-12-06 20:26:17 -0600

    Setup Angler to get Tipsy
    Change-Id: Ic5de01848c32ecf675b25a11ca367b09f4639394

--- tipsy.mk
+++ tipsy.mk
@@ -24,14 +24,14 @@ PRODUCT_RELEASE_NAME := angler
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
 PRODUCT_DEVICE := angler
-PRODUCT_NAME := slim_angler
+PRODUCT_NAME := tipsy_angler
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
