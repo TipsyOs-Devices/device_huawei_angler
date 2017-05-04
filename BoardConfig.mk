03b7effd7d9dbe37ce283fe25d193bde2b512ad6
ezio84 <brabus84@gmail.com>
2017-05-04 11:09:09 +0200

    Ignore vendor partition audio_effects.conf and use the device one
    needs https://github.com/ezio84/abc-n_frameworks_av/commit/dc1bf55f6954b3a94cb0c6298466703b8ec6233d
    
    Change-Id: I9aae76600abe2e28a3f7199ff8ae52907dd655d3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ USE_CLANG_PLATFORM_BUILD := true
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
-# DSP
-TARGET_NO_DSPMANAGER := true
+# Ignore vendor partition audio_effects.conf and use the device (system/etc/) one
+TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF := true
 
 -include vendor/huawei/angler/BoardConfigVendor.mk
